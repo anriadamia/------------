d1023d2f05b37a00dac586dca31ac5d9fcb7781e
anriadamia <anriadamia2000@gmail.com>
2022-10-17 20:31:22 +0400

    Javascript homework I

---
+++ scripts.js
@@ -0,0 +1,77 @@
+// let user = {
+//     firstname: "giorgi",
+//     lastname: "smith",
+//     age: 25,
+//     studentstatus: "active"
+//   };
+// console.log(user.studentstatus);  
+
+// let valueNames=["giorgi", "smith", 25, "active"];
+// for(let i=0; i<valueNames.length; i++){
+// console.log(valueNames[i]);}
+// //პირველი მეთოდი
+
+// for(let x of valueNames){
+//     console.log(x)
+// }
+// //მეორე  მეთოდი
+
+// let y=0
+// while(y<valueNames.length){
+//     console.log(valueNames[y]);
+//     y++;
+// }
+
+// let numbers = [5, 6, 10, 25, 3, 7, 4, 100, 28, 36, 89];
+// for(let z of numbers){
+//     if(z>5){
+//         console.log(z);
+//     }
+// }
+
+// let user = {
+//     name: 'giorgi',
+//     age:  20,
+//     studentstatus: 'active'
+// }
+
+// if(user.age<18 && user.studentstatus=="active"){
+//     console.log("Hello");
+// }else if(user.name=="levan"){
+//     console.log("Hello Levan")
+// }
+// else if(user.studentstatus=="active" || user.age<25){
+//     console.log("Hello World")
+// }
+// else{
+//     console.log("error")
+// }
+
+// let array = [ 'watermelon', 'pear', 10, 45, 50, 'apple', 'ananas' ];
+
+// for(let words of array){
+//     if(typeof words=="string"){
+//         console.log(words)
+//     }
+//     }
+
+// let array = [ 2, 3, 5, 10,25,24,11, 100 , 6,7, 10 ]
+// for(let a of array){
+//     if(a%2==0){
+//  console.log(a)
+// }
+// };
+// let array = [ 2, 3, 5, 10,25,24,11, 100 , 6,7, 10 ]
+// for(let a of array){
+//     if(a%2!==0){
+//  console.log(a)
+// }
+// }
+
+// let users = [
+//     {username: 'giorgi', status: false},
+//     {username: 'levani', status: false},
+//     {username: 'anna', status: true}
+// ]
+// console.log( users[2].status)
+   
